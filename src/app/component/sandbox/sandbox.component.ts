7bb512a67b5b6c96bc2584aa4ed573a42df34251
GOSSII <dipsgos7@gmail.com>
2018-10-12 16:35:43 -0400

    seprating HTML, CSS and TS

---
+++ src/app/component/sandbox/sandbox.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { unescapeIdentifier } from '@angular/compiler';
+
+@Component({
+    selector : 'sandbox',
+    templateUrl : `sandbox.component.html`,
+    styleUrls : [`sandbox.component.css`]
+})
+
+export class sandboxComponent{
+
+}
